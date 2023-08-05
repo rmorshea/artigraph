9dd0d93d31ceda0b000356f95e7b53dc59737f00
Ryan Morshead <ryan.morshead@gmail.com>
2023-08-04 21:53:01 -0600

    more type anno fixes

--- src/artigraph/orm/node.py
+++ src/artigraph/orm/node.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from datetime import datetime
-from typing import Any, ClassVar, Optional
+from typing import Any, ClassVar
 
 from sqlalchemy import ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column
